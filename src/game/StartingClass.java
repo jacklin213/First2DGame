8594f33075d2bb861d0d7a779cade7a35bead366
jack lin <jacklin213@gmail.com>
2014-08-24 18:23:34 +1200

    Basic Framework

--- src/game/StartingClass.java
+++ src/game/StartingClass.java
@@ -1,8 +1,48 @@
 package game;
 
 import java.applet.Applet;
+import java.awt.Graphics;
 
-public class StartingClass extends Applet {
+public class StartingClass extends Applet implements Runnable{
 
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
 	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		super.start();
+	}
+	
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
+	}
+	
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		super.update(g);
+	}
+
+	@Override
+	public void run() {
+		
+	}
 }
