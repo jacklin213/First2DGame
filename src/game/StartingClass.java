7d4ac1f3680a216593ac788963bc5cff5864528d
jack lin <jacklin213@gmail.com>
2014-08-27 22:26:12 +1200

    Added ball class

--- src/game/StartingClass.java
+++ src/game/StartingClass.java
@@ -1,5 +1,7 @@
 package game;
 
+import game.entity.Ball;
+
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -9,16 +11,7 @@ public class StartingClass extends Applet implements Runnable{
 
 	private Image image;
 	private Graphics doubleBuffer;
-	
-	int x = 200;
-	int y = 25;
-	int radius = 20;
-	double speedX = 20;
-	double speedY = 0;
-	double gravity = 15;
-	double energyloss = .65;
-	double xFriction = .9;
-	double dt = .2;
+	private Ball ball, badBall;
 	
 	@Override
 	public void init() {
@@ -27,6 +20,8 @@ public class StartingClass extends Applet implements Runnable{
 	
 	@Override
 	public void start() {
+		ball = new Ball();
+		badBall = new Ball(400, 50, Color.BLACK);
 		Thread thread = new Thread(this);
 		thread.start();
 	}
@@ -43,8 +38,8 @@ public class StartingClass extends Applet implements Runnable{
 	
 	@Override
 	public void paint(Graphics g) {
-		g.setColor(Color.GREEN);
-		g.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+		ball.paint(g);
+		badBall.paint(g);
 	}
 	
 	@Override
@@ -66,33 +61,8 @@ public class StartingClass extends Applet implements Runnable{
 	@Override
 	public void run() {
 		while(true) {
-			if (x + speedX > this.getWidth() - radius - 1) { //Checks if ball is touching wall. 1 because of pixal
-				x = this.getWidth() - radius - 1; 
-				speedX = -speedX; //Makes ball bounce off ball
-			} else if (x + speedX < 0 + radius) { 
-				x = 0 + radius;
-				speedX = -speedX; //Negative negative = positive
-			} else {
-				x += speedX;
-			}
-			
-			if (y == this.getHeight() - radius - 1) {
-				speedX *= xFriction;
-				if (Math.abs(speedX) < .8) {
-					speedX = 0;
-				}
-			}
-			
-			if (y > this.getHeight() - radius - 1) {
-				y = this.getHeight() - radius - 1;
-				speedY *= energyloss; //Make ball lose some energy. speedY = speedY * energyloss
-				speedY = -speedY;
-			} else {
-				speedY += gravity*dt; //v = u + at
-				y += speedY*dt + .5*gravity*dt*dt; // v = ut + 1/2at^2
-			}
-			
-			
+			ball.update(this);
+			badBall.update(this);
 			repaint();
 			try {
 				Thread.sleep(17);
---
+++ src/game/entity/Ball.java
@@ -0,0 +1,67 @@
+package game.entity;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import game.StartingClass;
+
+public class Ball {
+
+	private int x = 200;
+	private int y = 25;
+	private int radius = 20;
+	private double speedX = 20;
+	private double speedY = 0;
+	private double gravity = 15;
+	private double energyloss = .65;
+	private double xFriction = .9;
+	private double dt = .2;
+	private Color color;
+	
+	public Ball() {
+		this(200, 25);
+	}
+	
+	public Ball(int x, int y) {
+		this(x, y, Color.GREEN);
+	}
+	
+	public Ball(int x, int y, Color color) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+	
+	public void update(StartingClass instance) {
+		if (x + speedX > instance.getWidth() - radius - 1) { //Checks if ball is touching wall. 1 because of pixal
+			x = instance.getWidth() - radius - 1; 
+			speedX = -speedX; //Makes ball bounce off ball
+		} else if (x + speedX < 0 + radius) { 
+			x = 0 + radius;
+			speedX = -speedX; //Negative negative = positive
+		} else {
+			x += speedX;
+		}
+		
+		if (y == instance.getHeight() - radius - 1) {
+			speedX *= xFriction;
+			if (Math.abs(speedX) < .8) {
+				speedX = 0;
+			}
+		}
+		
+		if (y > instance.getHeight() - radius - 1) {
+			y = instance.getHeight() - radius - 1;
+			speedY *= energyloss; //Make ball lose some energy. speedY = speedY * energyloss
+			speedY = -speedY;
+		} else {
+			speedY += gravity*dt; //v = u + at
+			y += speedY*dt + .5*gravity*dt*dt; // v = ut + 1/2at^2
+		}
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(color);
+		g.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+	}
+}
